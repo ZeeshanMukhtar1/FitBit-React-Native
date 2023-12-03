82026a9350d04c89d698e8c4a3705b6c8946297c
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-12-03 12:17:07 -0800

    🍱 added assest & demo routes

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function index() {
+  return (
+    <View>
+      <Text>our first screen goes here</Text>
+    </View>
+  );
+}
